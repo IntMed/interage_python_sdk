de6ddf2232ee9b92981cc4a6b38c7dfddb03ab3d
Lucas Weyne <weynelucas@gmail.com>
2017-02-27 10:40:18 -0300

    Remoção dos comentários do template de setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import find_packages
 
 setup(
   name = 'interage_python_sdk',
-  packages = find_packages(), # this must be the same as the name above
+  packages = find_packages(),
   version = '0.1.0',
   description = 'SDK oficialmente mantido pela IntMed Software para auxiliar no desenvolvimento de aplicações em Python integradas ao serviço de interações medicamentosas do sistema Interage',
   author = 'Lucas Weyne',
   author_email = 'weynelucas@gmail.com',
-  url = 'https://github.com/weynelucas/interage_python_sdk', # use the URL to the github repo
-  download_url = 'https://github.com/weynelucas/interage_python_sdk/archive/0.1.0.tar.gz', # I'll explain this in a second
-  keywords = ['sdk', 'interage', 'api', 'intmed'], # arbitrary keywords
+  url = 'https://github.com/weynelucas/interage_python_sdk',
+  download_url = 'https://github.com/weynelucas/interage_python_sdk/archive/0.1.0.tar.gz',
+  keywords = ['sdk', 'interage', 'api', 'intmed'],
   classifiers = [],
 )

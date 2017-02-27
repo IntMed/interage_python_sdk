05bfd6186dd82f106541c0253d21ae3a0e956160
Lucas Weyne <weynelucas@gmail.com>
2017-02-27 09:13:40 -0300

    Ajustando numero de versão em setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import find_packages
 setup(
   name = 'interage_python_sdk',
   packages = find_packages(), # this must be the same as the name above
-  version = '0.1',
-  description = 'SDK para desenvolvimento de aplicações em Python integradas com a API de interações medicamentosas do Interage criada pela IntMed Software',
+  version = '0.1.0',
+  description = 'SDK oficialmente mantido pela IntMed Software para auxiliar no desenvolvimento de aplicações em Python integradas ao serviço de interações medicamentosas do sistema Interage',
   author = 'Lucas Weyne',
   author_email = 'weynelucas@gmail.com',
   url = 'https://github.com/weynelucas/interage_python_sdk', # use the URL to the github repo
-  download_url = 'https://github.com/weynelucas/interage_python_sdk/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/weynelucas/interage_python_sdk/archive/0.1.0.tar.gz', # I'll explain this in a second
   keywords = ['sdk', 'interage', 'api', 'intmed'], # arbitrary keywords
   classifiers = [],
 )

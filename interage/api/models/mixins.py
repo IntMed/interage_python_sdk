8beeb70688d7623501756b9c743cacb1296cc5c0
Lucas Weyne <lucas.weyne@lsbd.ufc.br>
2017-03-17 10:54:44 -0300

    Implementando create_instance_from_json no mixin

--- interage/api/models/mixins.py
+++ interage/api/models/mixins.py
@@ -1,4 +1,19 @@
 class CreateInstanceFromJsonMixin:
+    serializable_attrs = ['serializable_name', 'serializable_ref']
     @classmethod
     def create_instance_from_json(cls, json):
-        raise NotImpementedError()
+        instance = cls()
+        properties = [prop for name, prop in cls.__dict__.items() if isinstance(prop, property)]
+        for p in properties:
+            if(cls.__has_serializable_properties):
+                serializable_ref = p.fget.serializable_ref
+                serializable_name = p.fget.serializable_name
+
+                if(serializable_ref is None)
+                    p.fset(instance, json[serializable_name])
+                else:
+                    p.fset(instance, serializable_ref.create_instance_from_json(json[serializable_name]))
+
+    @classmethod
+    def __has_serializable_properties(cls, prop):
+        return all([hasattr(prop.fget, attr) for attr in cls.serializable_attrs])
